416ac77292fb50206c8ba4d9996c3047cce4dae7
dennis-sketch <dennis.kimani2@student.moringaschool.comgit config --global user.name dennis-sketch>
2023-11-21 20:01:41 +0300

    completed task

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
+    greet_programmer()
 
 def greet(name):
-    pass
+    print("Hello, {name}!")
+    greet("Sunny")
 
 def greet_with_default(name="programmer"):
-    pass
+    print("Hello, {name}!")
+    greet_with_default("Sunny")
+    greet_with_default()
 
 def add(num1, num2):
-    pass
+    result=(2 + 2)
+    print(4)
 
 def halve(number):
-    pass
+    result=(4/2)
+    print(2)
